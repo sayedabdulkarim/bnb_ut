06cf67da29d2e148b56d5530d3d903448a762634
unknown <sakarim9124@gmail.com>
2024-03-12 00:47:59 +0530

    updated

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,9 +1,22 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 import React from "react";
 import { useDispatch } from "react-redux"; // Import useDispatch
 import { setAuthenticated } from "@/slices/authSlice";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
 
 const Login = () => {
+  //misc
+  const router = useRouter();
   const dispatch = useDispatch(); // Get the dispatch function
 
   const handleLogin = () => {
@@ -12,8 +25,40 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <Button title="Login" onPress={handleLogin} />
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.seperatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "black",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.seperator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "black",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={[defaultStyles.btn, { backgroundColor: "green" }]}
+        onPress={handleLogin}
+      >
+        <Text style={defaultStyles.btnText}>Continue as Guest</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -23,7 +68,35 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 26,
+  },
+
+  seperatorView: {
+    flexDirection: "row",
+    gap: 10,
     alignItems: "center",
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: "mon-sb",
+    color: Colors.grey,
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: "#000",
+    fontSize: 16,
+    fontFamily: "mon-sb",
   },
 });
